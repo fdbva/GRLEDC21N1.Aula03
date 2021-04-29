40c622fc796532ee5edf605a432017a023aee8b2
fdbva <villela.felipe@gmail.com>
2021-04-28 21:04:01 -0300

    Add project files.

---
+++ Domain/SellService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SellService
+    {
+        public void Pay(
+            //idUsuario que veio da request
+            )
+        {
+            //pegar informações do usuário
+
+            //determinar meio de pagamento
+            //cartão de crédito
+
+            //validar cartão de crédito
+
+            //verificar se tem o produto em estoque
+            //reservar em estoque
+
+            //cobrar do cartão
+
+            //decrementar do estoque
+
+            //enviar email para usuario
+
+            //retornar relatório pro usuário
+        }
+
+        public void PayAfter()
+        {
+            //pegar informações do usuário
+
+            //determinar meio de pagamento
+            //cartão de crédito
+
+            //validar cartão de crédito
+
+            //verificar se tem o produto em estoque
+
+            //cobrar do cartão
+
+            //decrementar do estoque
+
+            //enviar email para usuario
+
+            //retornar relatório pro usuário
+        }
+    }
+}
